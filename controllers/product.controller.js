a34336a4f8bb839dfa14d4a6eae5fbdb028fdf41
zzgh06 <zzgh06@naver.com>
2024-06-05 16:58:53 +0900

    search BE

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -23,8 +23,13 @@ productController.createProduct = async (req, res) => {
 
 productController.getProducts = async (req, res) => {
   try {
-    const products = await Product.find({});
-    res.status(200).json({ status : 'success', products });
+    const { page, name } = req.query
+    const cond = name ? {name:{$regex:name, $options:"i"}} : {}
+    // 선언
+    let query = Product.find(cond);
+    // 실행
+    const productList = await query.exec();
+    res.status(200).json({ status : 'success', productList });
   } catch (error) {
     res.status(400).json({ status : 'fail', error : error.message });
   }
